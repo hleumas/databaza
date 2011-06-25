2c24e8e850d22c037a18e0bf6376048e7098f867
Samuel <samuel@fks.sk>
2011-06-25 19:44:10 +0200

    Snaha trochu zmensit vazbu medzi css a js v gridite

--- www/js/jquery.ui.gridito.js
+++ www/js/jquery.ui.gridito.js
@@ -11,27 +11,8 @@ $.widget("ui.gridito", {
 		var _this = this;
 		
 		this.table = this.element.find("table.gridito-table");
-		this.table.addClass("ui-widget ui-widget-content");
-		this.table.find("th").addClass("ui-widget-header");
-		this.table.find("tbody tr").hover(function () {
-			$(this).addClass("ui-state-hover");
-		}, function () {
-			$(this).removeClass("ui-state-hover");
-		});
-		
-		// sorting icons
-		function initSortingIcons(normalClass, hoverClass) {
-			_this.table.find("thead th ." + normalClass).hover(function () {
-				$(this).removeClass(normalClass).addClass(hoverClass);
-			}, function () {
-				$(this).removeClass(hoverClass).addClass(normalClass);
-			});
-		};
-
-		initSortingIcons("ui-icon-carat-2-n-s", "ui-icon-triangle-1-n");
-		initSortingIcons("ui-icon-triangle-1-n", "ui-icon-triangle-1-s");
-		initSortingIcons("ui-icon-triangle-1-s", "ui-icon-carat-2-n-s");
-
+        this.table.addClass("ui-widget ui-widget-content");
+        //
 		// buttons
 		this.element.find("a.gridito-button").each(function () {
 			var el = $(this);
@@ -74,3 +55,7 @@ $.widget("ui.gridito", {
 });
 
 })(jQuery);
+
+$("div.gridito").livequery(function () {
+$(this).gridito();
+});
