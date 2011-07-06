d2267a38c192fd0b62170e65c1f6c882a1608891
Samuel Hapak <samuel.hapak@gmail.com>
2011-07-07 01:06:00 +0200

    Get part of RiesitelSource

--- app/models/database/RiesitelSource.php
+++ app/models/database/RiesitelSource.php
@@ -15,9 +15,12 @@
  */
 
 use \Nette\Database\Connection;
-class RiesitelSource implements ISource
+class RiesitelSource extends CommonSource
 {
-    private $_dbConnection;
+    private $adresaSrc;
+    private $skolaSrc;
+    private $osobaSrc;
+    private $studiaSrc;
     /**
      * Source constructor
      *
@@ -25,7 +28,16 @@ class RiesitelSource implements ISource
      */
     public function __construct($dbConnection)
     {
-        $this->_dbConnection = $dbConnection;
+        parent::__construct($dbConnection);
+        $this->adresaSrc = new AdresaSource($dbConnection);
+        $this->skolaSrc  = new skolaSource($dbConnection);
+        $this->osobaSrc  = new OsobaSource($dbConnection);
+        $this->studiaSrc = new TypStudiaSource($dbConnection);
+    }
+
+    protected function getTable()
+    {
+        return 'riesitel';
     }
 
     /**
@@ -37,8 +49,24 @@ class RiesitelSource implements ISource
      */
     public function getById($id)
     {
-        $query = 'SELECT ';
-        $record = new Riesitel();
+        $data = array();
+        $fetch = $this->whereId($id)->fetch();
+        if ($fetch === false) {
+            throw new InvalidIdException("Id $id was not found in database");
+        }
+        $data = FlatArray::toArray($fetch);
+
+        foreach (array(
+            'osoba' => $this->osobaSrc,
+            'skola' => $this->skolaSrc,
+            'koresp_adresa' => $this->adresaSrc,
+            'typ_studia' => $this->studiaSrc) as $property => $source) {
+            if ($data["{$property}_id"]) {
+                $data[$property] = $source->getById($data["{$property}_id"]);
+            }
+        }
+
+        return new RiesitelRecord($data);
     }
 
     /**
@@ -46,7 +74,7 @@ class RiesitelSource implements ISource
      *
      * @param IRecord $record
      */
-    public function insert($record)
+    public function insertDb($record)
     {
     }
 
@@ -55,7 +83,7 @@ class RiesitelSource implements ISource
      *
      * @param IRecord $record
      */
-    public function update($record)
+    public function updateDb($record)
     {
     }
 
