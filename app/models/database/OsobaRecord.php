5a54923c5206c7833983e44b40d7fc235164a4a1
Samuel Hapak <samuel.hapak@gmail.com>
2011-07-06 19:00:59 +0200

    Add Osoba and Riesitel records

--- app/models/database/OsobaRecord.php
+++ app/models/database/OsobaRecord.php
@@ -18,7 +18,7 @@ use \Nette\Utils\Strings;
 class OsobaRecord extends CommonRecord
 {
 
-    protected $_data = array(
+    protected $_fields = array(
         'id',
         'meno',
         'priezvisko',
@@ -29,13 +29,12 @@ class OsobaRecord extends CommonRecord
         'jabber'
     );
 
-    protected $_object = array('adresa');
+    protected $_object = array('AdresaRecord' => 'adresa');
     protected $_mandatory = array('meno', 'priezvisko');
 
     public function normalize()
     {
         parent::normalize();
-        $this->_data['telefon'] = self::normalize($this->_data['telefon']);
     }
     /**
      * Check the validity of data
@@ -46,7 +45,7 @@ class OsobaRecord extends CommonRecord
     {
         parent::validate();
         if (!is_null($this->_data['datum_narodenia']) 
-            && !instanceof(\Nette\DateTime) {
+            && !$this->_data['datum_narodenia'] instanceof \Nette\DateTime) {
                 throw new InvalidDataException("datum_narodenia must be an DateTime instance");
         }
 
@@ -54,12 +53,8 @@ class OsobaRecord extends CommonRecord
             throw new InvalidDataException('Invalid phone format');
         }
 
-        if (!\Nette\Forms\Controls\TextBase::validateEmail($this->_data['email']) {
+        if (!\Nette\Forms\Controls\TextBase::validateEmail($this->_data['email'])) {
             throw new InvalidDataException('Invalid email format');
-        }
-
-        if (!Strings::match($this->_data['psc'], '/^\s*([0-9]\s*){5}$/')) {
-            throw new InvalidDataException("{$this->_data['psc']} is not valid psc value");
         }
     }
 }
--- app/models/database/RiesitelRecord.php
+++ app/models/database/RiesitelRecord.php
@@ -17,10 +17,49 @@
 class RiesitelRecord extends CommonRecord
 {
 
+    const KORESP_DOMOV = 0;
+    const KORESP_SKOLA = 1;
+    const KORESP_ELSE  = 2;
+    protected $_fields = array(
+        'id',
+        'osoba',
+        'skola',
+        'rok_maturity',
+        'typ_studia',
+        'telefon_rodic',
+        'koresp_kam',
+        'koresp_adresa'
+    );
+
+    protected $_object = array('osobaRecord' => 'osoba', 'SkolaRecord' => 'skola', 'AdresaRecord' => 'koresp_adresa');
+    protected $_mandatory = array('osoba', 'skola', 'rok_maturity', 'typ_studia', 'koresp_kam');
     /**
      * Check the validity of data
      *
      * @throws InvalidDataException
      */
-    public function validate();
+    public function validate()
+    {
+        $data = $this->_data;
+        if (!is_numeric($data['rok_maturity'])
+            || $data['rok_maturity'] < 1980
+            || $data['rok_maturity'] > 2030) {
+            throw new InvalidDataException('Invalid rok_maturity format');
+        }
+
+        if (!self::isPhoneValid($data['telefon_rodic'])) {
+            throw new InvalidDataException('Invalid telefon_rodic format');
+        }
+
+        if (!is_numeric($data['koresp_kam'])
+            || $data['koresp_kam'] < 0
+            || $data['koresp_kam'] > 2) {
+            throw new InvalidDataException('Invalid koresp_kam value');
+        }
+
+        if ($data['koresp_kam'] == self::KORESP_ELSE && is_null($data['koresp_adresa'])) {
+            throw new InvalidDataException('koresp_adresa must be set with KORESP_ELSE');
+        }
+
+    }
 }
