b28ad5188bf9fee649925a2b583606a1d1de18be
Samuel Hapak <samuel.hapak@gmail.com>
2011-07-07 14:33:50 +0200

    Separate NeonGriditoFactory

--- app/presenters/ZoznamyPresenter.php
+++ app/presenters/ZoznamyPresenter.php
@@ -80,51 +80,38 @@ class ZoznamyPresenter extends BasePresenter
         if (!is_file($file) || !is_readable($file)) {
             throw new Nette\FileNotFoundException("File $file is missing or is not readable.");
         }
-        $settings = Neon::decode(file_get_contents($file));
 
-        /** Create Grid and set the model */
-        $grid = new Grid();
-        $grid->setModel(new NetteModel($this->getContext()
-                ->getService('database')->table($settings['table'])));
+        $grid = NeonGriditoFactory::createGrid(
+            $this->context->database,
+            file_get_contents($file));
 
         /** Create form */
         $grid->addComponent($this->createForm($type), "form");
         $form = $grid['form'];
 
-        /** Add columns */
-        foreach ($settings['columns'] as $key => $column) {
-            $grid->addColumn($key, null, $column);
-        }
-
         /** Set button handlers */
-        $but = &$settings['buttons'];
         $getData = callback($this, 'getData' . ucfirst($type));
-        $but['delete']['handler'] = callback($this, 'delete' . ucfirst($type));
+        $grid['actions']->getComponent('delete')->handler = 
+            callback($this, 'delete' . ucfirst($type));
 
-        $but['detail']['handler'] = function($row) use ($form, $getData) {
+        $grid['actions']->getComponent('detail')->handler = 
+            function($row) use ($form, $getData) {
             $form->setDefaults($getData->invoke($row['id']));
             $form->setRenderer(new DisplayFormRenderer);
             $form->render();
         };
 
-        $but['edit']['handler'] = function($row) use ($form, $getData) {
+        $grid['actions']->getComponent('edit')->handler =
+            function($row) use ($form, $getData) {
             $form->setDefaults($getData->invoke($row['id']));
             $form->render();
         };
 
-        $but['pridaj']['handler'] = function() use ($form) {
+        $grid['toolbar']->getComponent('pridaj')->handler =
+            function() use ($form) {
             $form->render();
         };
 
-        /** Create buttons */
-        foreach ($settings['buttons'] as $key => $button) {
-            $call  = empty($button['toolbar']) ? 'add' : 'addToolbar';
-            $call .= empty($button['window']) ? 'Button' : 'WindowButton';
-            unset($button['toolbar']);
-            unset($button['window']);
-
-            call_user_func(array($grid, $call), $key, null, $button);
-        }
 
 
         return $grid;
