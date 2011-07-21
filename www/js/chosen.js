8f0a426f15283ad9f091338359b8dd129b89aa54
Samuel Hapak <samuel.hapak@gmail.com>
2011-07-21 17:55:48 +0200

    precisteny chosen

--- www/js/chosen.js
+++ www/js/chosen.js
@@ -1,98 +1,113 @@
 $.extend({
         chosen: function(element) {
-                    this.data = new Array();
-                    this.selected = null;
-                    this.value = element.val();
-                    this.label = null;
-                    this.name = element.attr('name');
-                    this.parseOptions(element);
+                    var opt         = this.parseOptions(element);
+                    this.options    = opt.options;
+                    this.selected   = opt.selected;
+                    this.emptyLabel = opt.emptyLabel;
+                    this.emptyValue = opt.emptyValue;
+
+                    this.noMatch = element.attr('data-no-match')
+                                || 'Žiadna položka nevyhovuje filtru';
                     this.prefix = element.attr('id');
-                    this.control = this.createControl(this.prefix);
+                    this.createControl(this.prefix, element.attr('name'));
                     $(element).replaceWith(this.control);
+                    this.select(this.selected);
                     this.setWidth(this.drop.width() + 10);
                     this.drop.hide();
         },
         scrollIntoView: function(element) {
-            container = element.parent();
-            var scrollPosition = container.scrollTop();
-            var containerBottom = container.height(); 
-            var elemTop = element.position().top;
-            var elemBottom = elemTop + element.outerHeight(); 
+            var c          = element.parent(),
+                elemTop    = element.position().top,
+                elemBottom = elemTop + element.outerHeight(); 
             if (elemTop < 0) {
-                container.scrollTop(elemTop+scrollPosition);
-            } else if (elemBottom > containerBottom) {
-                container.scrollTop(elemBottom - containerBottom + scrollPosition);
+                container.scrollTop(elemTop+c.scrollTop());
+            } else if (elemBottom > c.height()) {
+                container.scrollTop(elemBottom - c.height() + c.scrollTop());
             }
         }
 });
 
-$.chosen.prototype.parseOptions = function (select) {
-    var data = this.data;
-    var choose = this;
+$.chosen.prototype.parseOptions = function(select) {
+    var options    = Array(),
+        selected   = null,
+        value      = select.val(),
+        emptyValue = select.attr('data-nette-empty-value'),
+        emptyLabel = '';
+
+    var parseOption = function(data, elem) {
+        var opt = {label: elem.text(), value: elem.val()};
+        if (opt.value !== emptyValue) {
+            opt.normalized = $.removeDiacritics(opt.label).toLowerCase();
+            data.push(opt);
+            return (opt.value === value);
+        } else {
+            emptyLabel = opt.label;
+            return false;
+        }
+    };
+
     select.children('option, optgroup').each(function() {
         var elem = $(this);
         if (elem.is('option')) {
-            var opt = {label: elem.text(), value: elem.val()};
-            opt.normalized = $.removeDiacritics(opt.label).toLowerCase();
-            var i = data.push(opt) - 1;
-            if (opt.value == choose.value) {
-                choose.selected = i;
-            }
-        } else if (elem.is('optgroup')) {
+            selected = parseOption(options, elem)
+                     ? '' + (options.length - 1) : selected;
+        } else {
             var group = {label: elem.attr('label'), value: new Array()};
-            var i = data.push(group) - 1;
+            var i = options.push(group) - 1;
             elem.children('option').each(function() {
-                var opt = $(this);
-                opt = {label: opt.text(), value: opt.val()};
-                opt.normalized = $.removeDiacritics(opt.label).toLowerCase();
-                var j = group['value'].push(opt) - 1;
-                if (opt.value == choose.value) {
-                    choose.selected = i + '_' + j;
-                    choose.label = opt.label;
-                }
+                selected = parseOption(options[i].value, $(this))
+                         ? i + '_' + (options[i].value.length - 1) : selected;
             });
         }
     });
+
+    return {
+        'options'   : options,
+        'selected'  : selected,
+        'emptyValue': emptyValue,
+        'emptyLabel': emptyLabel
+    };
 };
 
 $.chosen.prototype.filter = function(string) {
     string = $.removeDiacritics(string).toLowerCase();
-    for (var i in this.data) {
-        if (this.data[i].normalized == undefined) {
+    for (var i in this.options) {
+        if (this.options[i].normalized === undefined) {
             var visible = false;
-            for (var j in this.data[i].value) {
-                if (this.data[i].value[j].normalized.indexOf(string) == -1) {
-                    this.hide(i+'_'+j);
-                } else {
-                    visible = true;
-                    this.show(i+'_'+j);
-                }
+            for (var j in this.options[i].value) {
+                found = (this.options[i].value[j].normalized.indexOf(string) != -1);
+                visible = visible || found;
+                this.show(i+'_'+j, found);
             }
-            if (visible) {
-                this.show(i);
-            } else {
-                this.hide(i);
-            }
+            this.show(i, visible);
         } else {
-            if (this.data[i].normalized.indexOf(string) == -1) {
-                this.hide(i);
-            } else {
-                this.show(i);
-            }
+            this.show(i, (this.options[i].normalized.indexOf(string) != -1));
         }
     }
+    var noMatch = false;
     if (!this.drop.find('.highlighted').is(':visible')) {
+        var first = this.drop.find('li.active-result:visible:first');
+        if (first.length === 0) {
+            noMatch = true;
+        }
         this.highlight(this.drop.find('li.active-result:visible:first'));
+    }
+    if (noMatch) {
+        this.noMatch.show();
+    } else {
+        this.noMatch.hide();
     }
 
 };
 
-$.chosen.prototype.hide = function(index) {
-    $('#' + this.prefix + '_' + index).hide();
-};
-
-$.chosen.prototype.show = function(index) {
-    $('#' + this.prefix + '_' + index).show();
+$.chosen.prototype.show = function(index, show) {
+    show = (show === undefined || show === null) ? true : show;
+    var elem = $('#' + this.prefix + '_' + index);
+    if (show) {
+        elem.show();
+    } else {
+        elem.hide();
+    }
 };
 
 
@@ -106,141 +121,135 @@ $.chosen.prototype.createOption = function(id, label, group) {
     } else {
         li.addClass('active-result');
         li.mouseenter(function (e) {choose.highlight($(this));});
-        li.click(function(e) {choose.select($(this)); choose.rollIn();});
+        li.click(function(e) {choose.selectElem($(this)); choose.rollIn();});
     }
     return li;
 };
-$.chosen.prototype.createOptions = function (prefix) {
+$.chosen.prototype.createOptions = function(prefix) {
     var ul = $("<ul class='chzn-results'></ul>");
-    var data = this.data;
-    var choose = this;
-    for (var i in data) {
-        var elem = data[i];
+    for (var i in this.options) {
+        var elem = this.options[i];
         if ($.isArray(elem.value)) {
             var li = this.createOption(prefix + '_' + i, elem.label, true);
             ul.append(li);
             for (var j in elem.value) {
-                var subelem = elem.value[j];
-                var subli = this.createOption(prefix + '_' + i + '_' + j, subelem.label, false);
-                subli.addClass('group-option');
-                ul.append(subli);
+                var li = this.createOption(prefix + '_' + i + '_' + j, elem.value[j].label, false);
+                li.addClass('group-option');
+                ul.append(li);
             }
         } else {
             var li = this.createOption(prefix + '_' + i, elem.label, false);
             ul.append(li);
         }
     }
+    var noMatch = $('<li class="no-results"></li>');
+    noMatch.text(this.noMatch);
+    this.noMatch = noMatch;
+    this.noMatch.hide();
+    ul.append(this.noMatch);
     return ul;
 };
 
-$.chosen.prototype.createControl = function (prefix) {
-    var container = $('<div class="chzn-container chzn-container-single"></div>');
-    container.attr('id', prefix);
-    this.hidden = $('<input type="hidden" />');
-    this.hidden.attr('name', this.name);
-    this.hidden.val(this.value);
-    container.append(this.hidden);
-    this.roller = $('<a class="chzn-single"></a>');
-    this.current = $('<span></span>');
-    this.current.text(this.label);
-    this.roller.append(this.current);
-    this.roller.append($('<div><b></b></div>'));
+$.chosen.prototype.createControl = function(prefix, name) {
     var choose = this;
-    this.roller.click(function(e) {
+
+    /** Create hidden field */
+    this.hidden = $('<input type="hidden" />').attr('name', name);
+
+    /** Create roller */
+    this.current = $('<span></span>');
+    this.roller = $('<a class="chzn-single"></a>')
+                .append(this.current)
+                .append($('<div><b></b></div>'))
+                .click(function(e) {
         if (choose.isRolled()) {
             choose.rollIn();
         } else {
             choose.rollOut();
         }
     });
-    container.append(this.roller);
-    this.drop = $('<div class="chzn-drop"></div>');
-    var search = $('<div class="chzn-search"></div>');
-    this.search = $('<input type=text />');
-    search.append(this.search);
-    this.search.change(function() {
+
+    /** Crate search input */
+    this.search = $('<input type=text />').change(function() {
         choose.filter($(this).val());
-    });
-    this.search.keyup(function() {
+    }).keyup(function() {
         $(this).change();
-        console.log('pruser');
-    });
-    this.search.click(function() {
+    }).click(function() {
         $(this).change();
     });
-    this.options = this.createOptions(prefix);
-    this.drop.append(search);
-    this.drop.append(this.options);
-    container.append(this.drop);
-    container.keydown(function(e){
-        if (e.keyCode == 38) {
+
+    /** Create dropdown */
+    this.drop = $('<div class="chzn-drop"></div>')
+              .append($('<div class="chzn-search"></div>').append(this.search))
+              .append(this.createOptions(prefix));
+
+    /** Create control */
+    this.control = $('<div class="chzn-container chzn-container-single"></div>')
+                 .attr('id', prefix)
+                 .append(this.hidden)
+                 .append(this.roller)
+                 .append(this.drop)
+                 .keydown(function(e){
+        if (e.keyCode === 38) {
             if (choose.isRolled()) {
                 choose.moveUp();
             } else {
                 choose.rollOut();
             }
-        } else if (e.keyCode == 40) {
+            e.preventDefault();
+        } else if (e.keyCode === 40) {
             if (choose.isRolled()) {
                 choose.moveDown();
             } else {
                 choose.rollOut();
             }
-        } else if (e.keyCode == 27) {
+            e.preventDefault();
+        } else if (e.keyCode === 27) {
             choose.rollIn()
             e.preventDefault();
             e.stopImmediatePropagation();
-        } else if (e.keyCode == 13) {
-            choose.select(choose.drop.find('.highlighted'));
+        } else if (e.keyCode === 13) {
+            choose.selectElem(choose.drop.find('.highlighted'));
             choose.rollIn();
             e.preventDefault();
         }
     });
-
-    return container;
 };
 
-$.chosen.prototype.setWidth = function(width)
-{
+$.chosen.prototype.setWidth = function(width) {
     this.control.width(width);
     //this.roller.width(width-10);
     this.search.width(width-22);
     this.drop.width(width-2);
 };
 
-$.chosen.prototype.highlight = function (elem) {
-    if (elem.length != 1) {
-        return;
+$.chosen.prototype.highlight = function(elem) {
+    if (elem.length === 1) {
+        this.drop.find('.highlighted').removeClass('highlighted');
+        elem.addClass('highlighted');
+        $.scrollIntoView(elem);
     }
-    this.options.find('.highlighted').removeClass('highlighted');
-    elem.addClass('highlighted');
-    $.scrollIntoView(elem);
 };
 
 $.chosen.prototype.isRolled = function() {
-    if (this.control.hasClass('chzn-container-active')) {
-        return true;
-    } else {
-        return false;
-    }
+    return this.control.hasClass('chzn-container-active');
 };
 $.chosen.prototype.rollOut = function() {
-    if (this.isRolled()) {
-        return;
+    if (!this.isRolled()) {
+        this.control.addClass('chzn-container-active');
+        this.roller.addClass('chzn-single-with-drop');
+        this.drop.show();
+        this.highlight(this.drop.find('#' + this.prefix + '_' + this.selected));
+        this.search.focus();
     }
-    this.control.addClass('chzn-container-active');
-    this.roller.addClass('chzn-single-with-drop');
-    this.drop.show();
-    this.highlight(this.drop.find('#' + this.prefix + '_' + this.selected));
-    this.search.focus();
 };
 
 $.chosen.prototype.rollIn = function() {
-    if (!this.isRolled()) {
-        return;
+    if (this.isRolled()) {
+        this.control.removeClass('chzn-container-active');
+        this.roller.removeClass('chzn-single-with-drop');
+        this.drop.hide();
     }
-    this.control.removeClass('chzn-container-active');
-    this.roller.removeClass('chzn-single-with-drop');
-    this.drop.hide();
 };
 
 $.chosen.prototype.moveUp = function() {
@@ -251,25 +260,37 @@ $.chosen.prototype.moveDown = function() {
     this.highlight(this.drop.find('.highlighted').nextAll('li.active-result:visible:first'));
 };
 
-$.chosen.prototype.select = function(elem) {
-    if (elem.length != 1) {
-        return;
-    }
-    this.selected = elem.attr('id').substring(this.prefix.length + 1);
-    var split = this.selected.indexOf('_');
-    if (split == -1) {
-        this.value = this.data[this.selected].value;
-    } else {
-        this.value = this.data[this.selected.substr(0, split)].value[this.selected.substr(split+1)].value;
-    }
-    this.hidden.val(this.value);
-    this.label = elem.text();
-    this.current.text(this.label);
+$.chosen.prototype.setValue = function(value) {
+    value = (value === null || value === undefined) ? this.emptyValue : value;
+    this.hidden.val(value);
 };
 
-$.chosen.prototy
+$.chosen.prototype.select = function(id) {
+    try {
+        if (id === null || id === undefined) {
+            throw 'empty';
+        }
+        index = id.split('_', 2);
+        var elem = this.options[index[0]];
+        if (index.length >= 2) {
+            elem = elem.value[index[1]];
+        }
+        this.current.text(elem.label);
+        this.setValue(elem.value);
+        this.selected = id;
+    } catch (err) {
+        this.setValue(null);
+        this.current.text(this.emptyLabel);
+        this.selected = null;
+    }
+    
+};
+$.chosen.prototype.selectElem = function(elem) {
+    if (elem.length === 1) {
+        this.select(elem.attr('id').substring(this.prefix.length + 1));
+    }
+};
 
 $(function() {
 $('select.chosen').livequery(function(){a = new $.chosen($(this));});
 });
-
