a295efba12742f7a2c427efb43250badb92bde77
Samuel Hapak <samuel.hapak@gmail.com>
2011-09-25 11:17:27 +0200

    Submit Module created

---
+++ app/SubmitModule/presenters/BasePresenter.php
@@ -0,0 +1,25 @@
+<?php
+namespace SubmitModule;
+
+/**
+ * My Application
+ *
+ * @copyright  Copyright (c) 2010 John Doe
+ * @package    MyApplication
+ */
+
+
+/**
+ * Base class for all application presenters.
+ *
+ * @author     John Doe
+ * @package    MyApplication
+ */
+abstract class BasePresenter extends \Nette\Application\UI\Presenter
+{
+    public function createComponentMenu()
+    {
+        return new \Menu(APP_DIR . '/SubmitModule/templates/menu.neon', 'menu');
+    }
+
+}
---
+++ app/SubmitModule/presenters/SignPresenter.php
@@ -0,0 +1,68 @@
+<?php
+namespace SubmitModule;
+
+/**
+ * My Application
+ *
+ * @copyright  Copyright (c) 2010 John Doe
+ * @package    MyApplication
+ */
+
+use Nette\Application\UI,
+	Nette\Security as NS;
+
+
+/**
+ * Sign in/out presenters.
+ *
+ * @author     John Doe
+ * @package    MyApplication
+ */
+class SignPresenter extends BasePresenter
+{
+
+
+	/**
+	 * Sign in form component factory.
+	 * @return Nette\Application\UI\Form
+	 */
+	protected function createComponentSignInForm()
+	{
+		$form = new UI\Form;
+		$form->addText('username', 'Login:')
+			->setRequired('Vyplňte prosím login.');
+
+		$form->addPassword('password', 'Heslo:')
+			->setRequired('Vyplňte prosím heslo.');
+
+		$form->addSubmit('send', 'Prihlásiť');
+
+		$form->onSuccess[] = callback($this, 'signInFormSubmitted');
+		return $form;
+	}
+
+
+
+	public function signInFormSubmitted($form)
+	{
+		try {
+			$values = $form->getValues();
+            $this->getUser()->setExpiration('+ 40 minutes', TRUE);
+			$this->getUser()->login($values->username, $values->password);
+			$this->redirect('Priklady:');
+
+		} catch (NS\AuthenticationException $e) {
+			$form->addError($e->getMessage());
+		}
+	}
+
+
+
+	public function actionOut()
+	{
+		$this->getUser()->logout();
+		$this->flashMessage('Boli ste odhlásení.');
+		$this->redirect('in');
+	}
+
+}
